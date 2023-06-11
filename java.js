92f0279394dfc40b71d98d68e33410acb0993b0e
facundogodoy1995 <facundogodoy1995@gmail.com>
2023-06-11 20:56:08 -0300

    primer repositorio

---
+++ java.js
@@ -0,0 +1,19 @@
+const nombre = "Facu"
+const edad = 27
+const variableConst = true 
+console.log(variableConst)
+
+if(variableConst){
+    console.log(typeof variableConst)
+}
+
+//if estructura de control para evaluar una condición
+// if(condicion{} lo que esta entre 2 llaves se llama bloque
+
+//let entrada = prompt("ingrese una letra")
+//let salida = entrada + " " + "ingresada"
+//alert(salida)
+
+let entrada = Number (prompt("ingrese un numero"))
+let salida = entrada + 1
+alert(entrada + 1)

8c0a65b3bd07a0c27eedef91f5ab4b9f40396d53
facundogodoy1995 <facundogodoy1995@gmail.com>
2023-07-10 23:35:38 -0300

    PreEntrega2Godoy

--- java.js
+++ java.js
@@ -1,27 +1,60 @@
-alert("Vamos a calcular el monto de tu compra")
-let producto1 = parseInt(prompt ("Ingrese el valor del primer producto"))
-let producto2 = parseInt(prompt ("Ingrese el valor del segundo producto"))
-let resultado = 0
+//PRE ENTREGA 1
 
-function costoTotal (Parametro1, Parametro2) {
+// alert("Vamos a calcular el monto de tu compra")
+// let producto1 = parseInt(prompt ("Ingrese el valor del primer producto"))
+// let producto2 = parseInt(prompt ("Ingrese el valor del segundo producto"))
+// let resultado = 0
+
+// function costoTotal (Parametro1, Parametro2) {
     
-    resultado = Parametro1 + Parametro2
+//     resultado = Parametro1 + Parametro2
+// }
+// costoTotal(producto1, producto2)
+
+
+// function mostrarCosto (resultadoCosto) {
+//     alert("el costo total es "+ "$" + resultadoCosto)
+// }
+
+// mostrarCosto(resultado)
+
+// let totalEnvio = resultado + 50
+// if (resultado<100) {
+//     alert("El valor es menor a $100, el producto tendrá un costo de envío de $50. El total mas el envío es: " + "$" + totalEnvio )
+// } else {
+//     alert("El valor es mayor a $100, el producto no tendrá costro de envío")
+// }
+
+
+
+//PRE ENTREGA 2
+
+class producto{
+    constructor(nombre, precio){
+        this.nombre = nombre
+        this.precio = precio
+        this.vendido = false
+    }
+    sumarIva(){
+        this.precio = this.precio * 1.21   
+    }
 }
-costoTotal(producto1, producto2)
+
+const producto3 = new producto ("perfume para ropa", 1200)
+
+const listaProducto = []
+console.log(listaProducto.length)
+let cantidad = Number(prompt("Ingrese cantidad de productos"))
+do{ 
+    let entrada = prompt("Ingresar nombre producto:")
+    listaProducto.push(entrada.toLocaleUpperCase())
+    console.log("listado", listaProducto)
+    console.log( "tamaño array:", listaProducto.length)
+}while(listaProducto.length !== cantidad)
 
 
-function mostrarCosto (resultadoCosto) {
-    alert("el costo total es "+ "$" + resultadoCosto)
-}
 
-mostrarCosto(resultado)
 
-let totalEnvio = resultado + 50
-if (resultado<100) {
-    alert("El valor es menor a $100, el producto tendrá un costo de envío de $50. El total mas el envío es: " + "$" + totalEnvio )
-} else {
-    alert("El valor es mayor a $100, el producto no tendrá costro de envío")
-}
 
 
 
@@ -208,3 +241,156 @@ if (resultado<100) {
 // }
 
 // console.log(calculadora(5,5,"+"));
+
+// arrow function
+
+// const sumar = (param1, param2) =>{
+//     console.log(param1 + param2)
+// }
+
+// sumar(5,5)
+
+// const suma = (a, b) => a + b   //tiene return
+
+// const calcularIva = valorProducto => valorProducto * 0.21
+
+// const calcularValorProducto = valor => {
+//     return valor + calcularIva(valor)
+// }
+
+// console.log(calcularValorProducto(100)) 
+
+// const persona = {
+//     nombre: "Homero",
+//     apellido:"Simpsons",
+//     edad:"35",
+//     email:"homero@gmail.com"
+
+// }
+
+// console.log(persona.nombre)
+// console.log(persona["nombre"])
+
+
+//funciones constructoras (Pascal case)
+
+
+// for (let i = 0; i<1; i++) {
+    //     const nombre = prompt("ingresar nombre")
+    //     const apellido = prompt("ingresar apellido")
+    //     const edad = parseInt( prompt("ingresar edad"))
+    // }
+    
+    function Personal (nombre, apellido, edad){
+        this.nombre   = nombre
+        this.apellido = apellido
+        this.edad     = edad
+    }
+//creando dos objetos (instancias)
+const persona3 = new Personal ("Lucas","Rodriguez", 25)
+const persona4 = new Personal ("Juan", "Perez", 35)
+
+
+console.log(persona3)
+console.log(persona4)
+
+const saludar = ()=>{
+    
+}
+
+const objeto = {
+    nombre: "faq",
+    edad: 28
+}
+
+//metodos (.log  .length  .lowercase)
+
+console.log(objeto)
+
+let texto = "Hola Coders"
+console.log(texto.length)
+console.log(texto.toLowerCase())
+
+function People(nombree, apellidoo, edadd) {
+    this.nombree   = nombree
+    this.apellidoo = apellidoo
+    this.hablar    = function () {
+        return("Hola soy" + this.nombree)
+    }
+}
+
+const people1 = new People("facu","faka")
+
+console.log(people1.hablar());
+
+//me enumeró las propiedades del objeto
+for (const propiedad in people1) {
+    console.log("propiedad: ", propiedad)
+    console.log("valor propiedad:", people1[propiedad])
+}
+
+// clases => suggar sintax => forma generica de escribir funciones constructoras
+
+class Pipol {
+    constructor(nombre, apellido, edad){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.edad=edad
+    }
+    talk(){      //metodo-function-va por fuera 
+        console.log("hola soy ", this.nombre)  
+    }
+}
+
+const humano = new Pipol ("juan", "perez", "29")
+console.log(humano)
+
+console.log(humano.talk)  //llamando a la funcion talk
+
+
+// class producto{
+//     constructor(nombre, precio){
+//         this.nombre = nombre
+//         this.precio = precio
+//         this.vendido = false
+//     }
+//     sumarIva(){
+//         this.precio = this.precio * 1.21   
+//     }
+// }
+
+// const producto3 = new producto ("arroz", 125)
+
+const arrayA = [1,2,"c3", true,{nombre:"facu"}]
+console.log(arrayA[4]) //para acceder utilizar la posicion entre corchetes
+
+// const numeros = [1,2,3,4,5,6]
+// for(let index=0; index<6; index++){
+//     alert(numeros[index])
+// }
+
+const miArray = ["facu", true,false]
+console.log(miArray.length)                //tamaño
+console.log(miArray[miArray.length - 1])   //ultima posicion
+// console.log/table/dir
+//.push agrega un dato mas al array miArray.push(3)
+//.unshift agrega un dato al principio miarray.unshift(true)
+//.shift elimina el primer elemento
+//.pop elimina el ultimo
+//.splice borra elementos
+//.join une los elementois de un array
+//.concat une 2 o mas arrays y elementos
+//.slice copiar desde hasta en un array
+//.indexOf devuelve la posicion dentro de un array de la primer coincidencia que encuentra
+//.includes() si un elemento que recibo por parámetro existe o no dentro de un array
+//.reverse() reversa los elementos
+
+// const listaProducto = []
+// console.log(listaProducto.length)
+// let cantidad = Number(prompt("Ingrese cantidad de productos"))
+// do{ 
+//     let entrada = prompt("Ingresar nombre producto:")
+//     listaProducto.push(entrada.toLocaleUpperCase())
+//     console.log("listado", listaProducto)
+//     console.log( "tamaño array:", listaProducto.length)
+// }while(listaProducto.length !== cantidad)
